704c369c4260e4e82d7d6d1db3369e496493c075
Lizz <lizwesterband@gmail.com>
2020-09-14 17:23:02 -0700

    did and undid

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -123,7 +123,7 @@ function App() {
 
           <Route path="/games/index"
           render={(props) => <GameIndex {...props} currentGame={currentGame} setCurrentGame={setCurrentGame}/>} />
-          <Route path="*" component={Error} />
+         
 
           <Route path="/games/:id"
 
@@ -131,7 +131,7 @@ function App() {
           setCurrentGame={setCurrentGame} currentUser={currentUser}
           setCurrentUser={setCurrentUser} setCurrentUserFaves={setCurrentUserFaves} />} />
 
-
+          <Route path="*" component={Error} />
 
 
         </Switch>
