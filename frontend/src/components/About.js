8cc62a92ce5be7fffa6de707cf13bf9604c600a9
blangwell <b.langwell@outlook.com>
2020-10-20 12:23:40 -0700

    [styles] Style About.js and edit JSX class/id names

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -4,13 +4,12 @@ import React from 'react';
 const About = () => {
     return (
         <div className="about">
-            <div className="head">
-            <h1>About</h1>
-            {/* <p>This is all about us.</p> */}
-            </div>
+            {/* <div className="head">
+            <p>This is all about us.</p>
+            </div> */}
+            <h1 className="page-title">About</h1>
             
             <div className="about-blurb">
-
                 <p>
                     <strong>Project One...</strong> seems like forever ago, right?
                     How many of us have revisited our games? Replayed our cohort mates games?
@@ -20,11 +19,16 @@ const About = () => {
                     play your classmates games as well! Maybe you're looking for something new? Well, devs from ALL cohorts will
                     have the ability to create a User account and upload their games as well! The idea behind this
                     was to create a place where we can all come together, support our previous works, and continue to develop, submit and grow!
-                    Questions? Comments? Wanna see more from us? Feel free to reach out to any of the devs via LinkedIn or check out our work on Github!<br />
-                    Martin Briceno: <a className="devs" target="_blank" href="https://github.com/mgustavob">Github </a><a className="devs" target="_blank" href="https://www.linkedin.com/in/martin-briceno-0825511b0/">LinkedIn</a><br />
-                    David Cabassa: <a className="devs" target="_blank" href="https://github.com/decpdx584">Github </a><a className="devs" target="_blank" href="https://www.linkedin.com/in/decpdx584/">LinkedIn</a><br />
-                    Barent Langwell: <a className="devs" target="_blank" href="https://github.com/blangwell">Github </a><a className="devs" target="_blank" href="https://www.linkedin.com/in/blangwell/">LinkedIn</a><br />
-                    Lizz West: <a className="devs" target="_blank" href="https://github.com/lizzwest">Github </a><a className="devs" target="_blank" href="https://www.linkedin.com/in/lizzwest/">LinkedIn</a>
+                    
+                    <br /><br />
+                        Questions? Comments? Wanna see more from us? Feel free to reach out to any of the devs via LinkedIn or check out our work on Github!
+                        <br /><br />
+                    <div id="dev-div">
+                        Martin Briceno: <a className="devs" target="_blank" href="https://github.com/mgustavob">Github </a><a className="devs" target="_blank" href="https://www.linkedin.com/in/martin-briceno-0825511b0/">LinkedIn</a><br />
+                        David Cabassa: <a className="devs" target="_blank" href="https://github.com/decpdx584">Github </a><a className="devs" target="_blank" href="https://www.linkedin.com/in/decpdx584/">LinkedIn</a><br />
+                        Barent Langwell: <a className="devs" target="_blank" href="https://github.com/blangwell">Github </a><a className="devs" target="_blank" href="https://www.linkedin.com/in/blangwell/">LinkedIn</a><br />
+                        Lizz West: <a className="devs" target="_blank" href="https://github.com/lizzwest">Github </a><a className="devs" target="_blank" href="https://www.linkedin.com/in/lizzwest/">LinkedIn</a>
+                    </div>
                 </p>
             </div>
         </div>
--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Landing = (props) => {
   return(
     <div id="landing">
-      <h1 className="pixel-text" id="landing-title">GArcade</h1>
+      <h1 className="pixel-text page-title">GArcade</h1>
       <h4 className="sub-title">A Place for Project One</h4><br />
       <img id="cabinet-far" src="assets/arcadeCab3.png"/><br />
       <br />
